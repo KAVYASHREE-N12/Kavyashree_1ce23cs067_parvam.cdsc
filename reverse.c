96b69d79d6dc106010ee29a7478c371ba64ebdcc
KAVYASHREE-N12 <nandukavya31@gmail.com>
2024-11-13 14:47:34 +0530

    Add files via upload

--- reverse.c
+++ reverse.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main(){
     int n;
+    printf("enter the size of array:");
     scanf("%d",&n);
-    int r=0;
-    while(n!=0){
-        int rem=n%10;
-        r=r*10+rem;
-        n=n/10;
+    int arr[n];
+    for( int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
     }
-    printf("%d",r);
-    return 0;
-}
+    printf("enter the array");
+    for(int i=n-1;i>=0;i--){
+        scanf("%d",arr[i]);
+        }
+        printf("enter the reversed array");
+        for(int i=n-1;i>=0;i--){
+            printf("%d",arr[i]);
+        }
